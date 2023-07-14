a8ad09d952309143e4a3df1e08e2898ce0d1bb0c
zakiapranti <63296880+zakiapranti@users.noreply.github.com>
2023-07-15 00:25:33 +0600

    Add files via upload

---
+++ our days are never cominng back.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+
+    string s="LIFE IS NOT A PROBLEM TO BE SOLVED";
+
+    cin>>N;
+    for (int i=0;i<N;i++){
+        cout<<s[i];
+    }
+    cout<<endl;
+    return 0;
+
+}
