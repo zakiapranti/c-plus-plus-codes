ff3130a8cf5f269dc17dc816aeb6bd4d6bee3e52
zakian pranty <63296880+zakiapranti@users.noreply.github.com>
2023-07-26 13:13:53 +0600

    Add files via upload

---
+++ ascending integers.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a,b,c;
+cin>>a>>b>>c;
+if (a<b&& a<c)
+    if (b<c)
+    cout<<a<<" "<<b<<" "<<c;
+    else
+    cout<<a<<" "<<c<<" "<<b;
+else if (b<a && b<c)
+    if (a<c)
+    cout<<b<<" "<<a<<" "<<c;
+    else
+    cout<<b<<" "<<c<<" "<<a;
+
+else if (c<a && c<b)
+    if (a<b)
+    cout<<c<<"  "<<a<<" "<<b;
+    else
+        cout<<c<<" "<<b<<" "<<a;
+
+
+
+
+    return 0;
+}
